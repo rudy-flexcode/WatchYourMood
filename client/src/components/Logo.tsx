2c41683c1d2ca99e82f27f1aeb32be49bb74358b
Manon Moutier <manon.moutier@hotmail.fr>
2024-11-07 18:25:13 +0100

    Nav and Logo components creation

---
+++ client/src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import "../App.css"
+
+function Logo() {
+  return (
+    <>
+      <img src="client/src/assets/images/Logo_WatchYourMood.png" alt="Logo WatchyourMood"/>
+    </>
+
+  );
+}
+
+export default Logo;
