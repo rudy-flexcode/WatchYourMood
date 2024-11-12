edfc65d26b99ff2ff2dd47b4a110f4753f72cfe4
rudy flexcode <rudyrobert2018@gmail.com>
2024-11-12 19:16:49 +0400

    com off

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-// import Avatar from "./components/Avatar";
+
 /* ************************************************************************* */
 
 // Import the main app component
