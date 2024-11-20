51eb70f4beb7bb87304cff761fd3bec2449eb233
Manon Moutier <manon.moutier@hotmail.fr>
2024-11-20 17:14:11 +0100

    tentative results mood by emotion/genre

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -5,7 +5,6 @@ import "./Movies.css";
 function Search() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [genres, setGenres] = useState([]); // Stocke la liste des genres
 
   useEffect(() => {
     // Récupérer les films
@@ -14,16 +13,7 @@ function Search() {
     )
       .then((response) => response.json())
       .then((json) => setMovies(json.results));
-
-    // Récupérer les genres
-    fetch(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=01e787d764d61219a648b30bc425cdc9&language=fr-FR",
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setGenres(data.genres);
       });
-  }, []);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
--- client/src/pages/MyMood.tsx
+++ client/src/pages/MyMood.tsx
@@ -1,8 +1,9 @@
 import { useParams } from "react-router-dom";
 import Logo from "../components/Logo";
-import Moovie from "../components/Moovies";
 import Nav from "../components/Navbar";
-import Search from "../components/Search";
+import EmotionsMovies from "../components/EmotionsMovies"
+
+
 
 function MyMood() {
   const { emotionID } = useParams(); //Récupére l'émotionID de l'URL
@@ -13,10 +14,7 @@ function MyMood() {
         <Nav isInMyMood={true} />
       </header>
       <main>
-        <Search />
-        <p> affiche films pour {emotionID}</p>{" "}
-        {/* affiche films pour l'émotion ID de l'url*/}
-        <Moovie />
+        <EmotionsMovies emotion={emotionID} />
       </main>
     </>
   );
