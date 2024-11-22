7341400e6a206dacbb3284108fcc33511250b5a8
Manon Moutier <manon.moutier@hotmail.fr>
2024-11-22 10:51:21 +0100

    fix biome

--- client/src/pages/MyMood.tsx
+++ client/src/pages/MyMood.tsx
@@ -50,7 +50,7 @@ function MyMood() {
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
-                />
+              />
               <h2>{movie.title}</h2>
             </div>
           ))}
