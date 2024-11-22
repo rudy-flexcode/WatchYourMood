5693dba1d1ba8b309337aed5065964ee2b46192c
rudy-flexcode <140716442+rudy-flexcode@users.noreply.github.com>
2024-11-22 12:24:43 +0400

    Update Avatar.tsx

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -10,39 +10,39 @@ const Avatar = ({
     <div className="blockAcceuil">
       {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
        <div className="block" data-name="joie">
-      <img
-        className="block1"
-        src={joieImage}
-        alt="vice versa joie"
-        onClick={() => handleEmotionClick("joie")}
-      />
+         <img
+         className="block1"
+         src={joieImage}
+         alt="vice versa joie"
+         onClick={() => handleEmotionClick("joie")}
+         />
        </div>
       {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
        <div className="block" data-name="tristesse">
-      <img
-        className="block2"
-        src={tristesseImage}
-        alt="vice versa triste"
-        onClick={() => handleEmotionClick("tristesse")}
-      />
+          <img
+          className="block2"
+          src={tristesseImage}
+          alt="vice versa triste"
+          onClick={() => handleEmotionClick("tristesse")}
+          />
       </div>  
       {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
       <div className="block" data-name="peur">
-      <img
+        <img
         className="block3"
         src={peurImage}
         alt="vice versa peur"
         onClick={() => handleEmotionClick("peur")}
-      />
+        />
       </div>  
       {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
       <div className="block" data-name="colere">
-      <img
+        <img
         className="block4"
         src={colereImage}
         alt="vice versa colère"
         onClick={() => handleEmotionClick("colere")}
-      />
+        />
       </div>  
     </div>
   );
